5209c67eb9dba965c68b4cb2d1a02ef96c61c3b5
Javier Ramirez <jimlombriz1988@gmail.com>
2018-07-30 11:48:34 -0500

    Update

--- app/src/main/java/mx/com/vialogika/dscdenunciaanonima/EditReport.java
+++ app/src/main/java/mx/com/vialogika/dscdenunciaanonima/EditReport.java
@@ -2,13 +2,29 @@ package mx.com.vialogika.dscdenunciaanonima;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditReport extends AppCompatActivity {
+
+    private EditText mName,MPosition,mSite,mClient,mEventDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_report);
+        init();
+        getItems();
+    }
+
+    private void init(){
         getSupportActionBar().hide();
+    }
+
+    private void getItems(){
+
+    }
+
+    private void setListeners(){
+
     }
 }
